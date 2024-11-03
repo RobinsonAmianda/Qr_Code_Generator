0fb22a085ff9b64222c7a0c4b9f8e2d5b022a755
RobinsonAmianda <robinsonamianda5@gmail.com>
2024-11-03 09:43:52 +0300

    Python file

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,3 +1,10 @@
 import qrcode
 
-
+# Generate QR code
+data = input("Enter the URL")
+file_name = input("Enter the file name: ")
+qr = qrcode.QRCode(box_size=10, border=4)
+qr.add_data(data)
+image = qr.make_image(fill_color = "black", back_color = "white")
+image.save(file_name + ".png")
+print("Image saved")
